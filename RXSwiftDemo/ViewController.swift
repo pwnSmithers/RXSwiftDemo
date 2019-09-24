fd9f409181aa0f693ee1a1726584ea795afa1612
Smith <smith.mugalu@andela.com>
2019-09-24 13:00:06 +0300

    Initial Commit

---
+++ RXSwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RXSwiftDemo
+//
+//  Created by Smithers on 24/09/2019.
+//  Copyright © 2019 Smithers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
